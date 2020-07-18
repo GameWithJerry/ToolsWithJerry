47d948b6106dee09a7094d42edd6cafb5b5d59b8
GameWithJerry <gamewithjerry@anglergames.de>
2020-07-18 13:08:31 +0200

    Update BedListener.java

--- src/de/gamewithjerry/autocrop/listener/BedListener.java
+++ src/de/gamewithjerry/autocrop/listener/BedListener.java
@@ -13,47 +13,37 @@ import org.bukkit.event.player.PlayerLoginEvent;
 public class BedListener implements Listener {
 
     int InBed = 0;
-    int OnlinePlayers = 0;
+    Server s = Bukkit.getServer();
 
     @EventHandler
     public void BedEnterListener(PlayerBedEnterEvent event) {
 
-
         Player p = event.getPlayer();
-        Server s = Bukkit.getServer();
 
         if(event.isCancelled() != true) {
             InBed++;
-            OnlinePlayers = 0;
 
             for(Player p2 : s.getOnlinePlayers()) {
-                OnlinePlayers++;
+                p2.sendMessage(ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "BedInfo" + ChatColor.DARK_GREEN + "] " + ChatColor.YELLOW + "Der Spieler " + p.getDisplayName() + " liegt nun im Bett. " + ChatColor.GOLD + "(" + ChatColor.YELLOW + InBed + ChatColor.GOLD + "/"+ ChatColor.YELLOW + s.getOnlinePlayers().size() + ChatColor.GOLD +")");
             }
 
-            for(Player p2 : s.getOnlinePlayers()) {
-                p2.sendMessage(ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "BedInfo" + ChatColor.DARK_GREEN + "] " + ChatColor.YELLOW + "Der Spieler " + p.getDisplayName() + " liegt nun im Bett. " + ChatColor.GOLD + "(" + ChatColor.YELLOW + InBed + ChatColor.GOLD + "/"+ ChatColor.YELLOW + OnlinePlayers + ChatColor.GOLD +")");
-            }
-
-            if((InBed / OnlinePlayers) >= 0.5) {
+            if(InBed / s.getOnlinePlayers().size() >= 0.5) {
                 s.getWorld("world").setTime(1000);
             }
         }
+
+        p.sendMessage(String.valueOf(InBed / s.getOnlinePlayers().size()));
     }
 
     @EventHandler
     public void BedLeaveListener(PlayerBedLeaveEvent event) {
 
         Player p = event.getPlayer();
-        Server s = Bukkit.getServer();
-
         InBed--;
-        OnlinePlayers = 0;
 
         if(p.getWorld().getTime() != 0) {
             for(Player p2 : s.getOnlinePlayers()) {
-                OnlinePlayers++;
-
-                p2.sendMessage(ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "BedInfo" + ChatColor.DARK_GREEN + "] " + ChatColor.YELLOW + "Der Spieler " + p.getDisplayName() + " hat sein Bett verlassen. " + ChatColor.GOLD + "(" + ChatColor.YELLOW + InBed + ChatColor.GOLD + "/"+ ChatColor.YELLOW + OnlinePlayers + ChatColor.GOLD +")");
+                p2.sendMessage(ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "BedInfo" + ChatColor.DARK_GREEN + "] " + ChatColor.YELLOW + "Der Spieler " + p.getDisplayName() + " hat sein Bett verlassen. " + ChatColor.GOLD + "(" + ChatColor.YELLOW + InBed + ChatColor.GOLD + "/"+ ChatColor.YELLOW + s.getOnlinePlayers().size() + ChatColor.GOLD +")");
             }
         }
     }
